5a8e32ef9c576a53fc42517196d0e602eca93d97
@MaxBalle <st178636@stud.uni-stuttgart.de>
2024-08-23 19:01:07 +0200

    Multi-style agents

--- fitness.py
+++ fitness.py
@@ -1,15 +1,18 @@
 from negotiationGenerator.scenario import ScenarioPerspective
+from main import time_cap
+
+#These are fitness functions for agents of different TKI negotiation styles. They can only be compared within a style, not across!
 
 def accommodating(outcome: tuple[int, float, float, int], negotiation_scenario_perspective: ScenarioPerspective) -> float:
     return __calc_fairness(outcome, time_weight=0.5, partner_utility_weight=1, joint_utility_weight=0.5)
 
 def collaborating(outcome: tuple[int, float, float, int], negotiation_scenario_perspective: ScenarioPerspective) -> float:
-    return __calc_fairness(outcome, time_weight=0.1, own_utility_weight=1, partner_utility_weight=1, joint_utility_weight=1, fairness_weight=1) #Fairness maybe 0.75
+    return __calc_fairness(outcome, time_weight=0.25, own_utility_weight=1, partner_utility_weight=1, joint_utility_weight=1, fairness_weight=1) #Fairness maybe 0.75
 
 def compromising(outcome: tuple[int, float, float, int], negotiation_scenario_perspective: ScenarioPerspective) -> float:
     return __calc_fairness(outcome, time_weight=0.5, own_utility_weight=0.5, partner_utility_weight=0.5, joint_utility_weight=0.5, fairness_weight=1)
 
-def avoiding_population(outcome: tuple[int, float, float, int], negotiation_scenario_perspective: ScenarioPerspective) -> float:
+def avoiding(outcome: tuple[int, float, float, int], negotiation_scenario_perspective: ScenarioPerspective) -> float:
     return __calc_fairness(outcome, time_weight=1, fairness_weight=1) #Fairness maybe 0.75, maybe some joint/own utility
 
 def competing(outcome: tuple[int, float, float, int], negotiation_scenario_perspective: ScenarioPerspective) -> float:
@@ -19,4 +22,4 @@ def __calc_fairness(outcome: tuple[int, float, float, int], time_weight=0.0, own
     result, own_utility, partner_utility, time = outcome
     joint_utility = own_utility + partner_utility
     fairness = abs(own_utility - partner_utility)
-    return own_utility_weight * own_utility + partner_utility_weight * partner_utility + joint_utility_weight * joint_utility + fairness_weight * fairness - time_weight * time
+    return own_utility_weight * own_utility + partner_utility_weight * partner_utility + joint_utility_weight * joint_utility + fairness_weight * fairness - time_weight * (time / time_cap)
--- main.py
+++ main.py
@@ -11,8 +11,6 @@ from multiprocessing import Pool
 from keras.src.models.cloning import clone_model
 import tensorflow as tf
 from keras import layers, Sequential
-#from tensorflow.python.keras import Sequential
-#import keras
 import numpy as np
 
 issues = [5, 5, 5, 5, 5]
@@ -23,7 +21,7 @@ survivor_count = 10 #Number of survivors per generation
 recombination_split = [10, 30, 50] #Top x to group and reproduce (2 -> 2), also has to be even
 mutation_stddev = 0.03
 
-def init_population(size: int) -> list[Agent]:
+def init_population(size: int, fitness_function) -> list[Agent]:
     population: list[Agent] = []
     for x in range(size):
         model = Sequential()
@@ -31,22 +29,22 @@ def init_population(size: int) -> list[Agent]:
         model.add(layers.GRU(50, name='Recurrent Middle', stateful=True))
         model.add(layers.Dense(28, name='Out', activation='sigmoid')) #First three are continue, accept and decline, the rest is values for a counteroffer
         model.build(input_shape=(1, 1, 50))
-        population.append(Agent(model))
+        population.append(Agent(model, fitness_function))
     return population
 
-def cross_negotiate(agent_1: Agent, agent_2: Agent, negotiation_scenario: Scenario, fitness_function_1, fitness_function_2):
+def cross_negotiate(agent_1: Agent, agent_2: Agent, negotiation_scenario: Scenario):
     #Negotiate in both constellations to decrease influence of unfair negotiations
     outcome_1_2 = negotiate(agent_1, agent_2, negotiation_scenario, time_cap)
     outcome_2_1 = negotiate(agent_2, agent_1, negotiation_scenario, time_cap)
-    fitness_1 = fitness_function_1(outcome_1_2, negotiation_scenario.a) + fitness_function_1(outcome_2_1, negotiation_scenario.b)
-    fitness_2 = fitness_function_2(outcome_1_2, negotiation_scenario.a) + fitness_function_2(outcome_2_1, negotiation_scenario.b)
+    fitness_1 = agent_1.fitness_function(outcome_1_2, negotiation_scenario.a) + agent_1.fitness_function(outcome_2_1, negotiation_scenario.b)
+    fitness_2 = agent_2.fitness_function(outcome_1_2, negotiation_scenario.a) + agent_2.fitness_function(outcome_2_1, negotiation_scenario.b)
     return fitness_1, fitness_2
 
 def reproduce(parent_1: Agent, parent_2: Agent) -> tuple[Agent, Agent]:
     genome_parent_1 = parent_1.model.get_weights()
     genome_parent_2 = parent_2.model.get_weights()
-    child_1 = Agent(clone_model(parent_1.model))
-    child_2 = Agent(clone_model(parent_2.model))
+    child_1 = Agent(clone_model(parent_1.model), parent_1.fitness_function)
+    child_2 = Agent(clone_model(parent_2.model), parent_2.fitness_function)
     genome_child_1 = []
     genome_child_2 = []
     for arr_1, arr_2 in zip(genome_parent_1, genome_parent_2):
@@ -76,11 +74,11 @@ def mutate(agent: Agent) -> Agent:
 if __name__ == "__main__":
     start_time = time.time()
     populations = {
-        "accommodation": init_population(population_size),
-        "collaborating": init_population(population_size),
-        "compromising": init_population(population_size),
-        "avoiding": init_population(population_size),
-        "competing": init_population(population_size)
+        "accommodating": init_population(population_size, fitness.accommodating),
+        "collaborating": init_population(population_size, fitness.collaborating),
+        "compromising": init_population(population_size, fitness.compromising),
+        "avoiding": init_population(population_size, fitness.avoiding),
+        "competing": init_population(population_size, fitness.competing)
     }
     generation = 1
     while generation <= max_generation:
@@ -89,34 +87,37 @@ if __name__ == "__main__":
         scenario: Scenario = build_negotiation_scenario(issues)
         print(f"Scenario:\nPerspective A:{scenario.a.get_issues()}\nPerspective B:{scenario.b.get_issues()}")
         #Negotiation simulation
-        random.shuffle(populations["collaborating"])
+        for population_name in populations:
+            random.shuffle(populations[population_name])
         #Intra-style negotiations
-        negotiations: list[tuple[Agent, Agent, Scenario, any, any]] = []
-        for i in range(0,population_size, 2):
-            negotiations.append((populations["collaborating"][i], populations["collaborating"][i+1], scenario, fitness.collaborating, fitness.collaborating))
+        negotiations: list[tuple[Agent, Agent, Scenario]] = []
+        for population_name in populations:
+            for i in range(0,population_size, 2):
+                negotiations.append((populations[population_name][i], populations[population_name][i+1], scenario))
         with Pool() as p:
             negotiation_results = p.starmap(cross_negotiate, negotiations)
         for negotiation, result in zip(negotiations, negotiation_results):
             negotiation[0].fitness += result[0]
             negotiation[1].fitness += result[1]
-        #Selection
-        populations["collaborating"].sort(key=lambda agent: agent.fitness)
-        print(f"Total fitness: {sum([agent.fitness for agent in populations['collaborating']])}")
-        print(f"Highest fitness: {populations['collaborating'][-1].fitness}")
-        new_population = []
-        new_population.extend(populations["collaborating"][-survivor_count:]) #Fittest survive into next generation
-        for agent in new_population: #Reset fitness of survivors
-            agent.fitness = 0.0
-        #Recombination and mutation
-        for group_size in recombination_split:
-            pool = populations["collaborating"][-group_size:]
-            random.shuffle(pool)
-            children = []
-            for i in range(0, group_size, 2):
-                children.extend(reproduce(pool[i], pool[i+1]))
-            for child in children:
-                new_population.append(mutate(child))
-        populations["collaborating"] = new_population
+        for population_name in populations:
+            #Selection
+            populations[population_name].sort(key=lambda agent: agent.fitness)
+            print(f"Total fitness {population_name}: {sum([agent.fitness for agent in populations[population_name]])}")
+            print(f"Highest fitness {population_name}: {populations[population_name][-1].fitness}")
+            new_population = []
+            new_population.extend(populations[population_name][-survivor_count:]) #Fittest survive into next generation
+            for agent in new_population: #Reset fitness of survivors
+                agent.fitness = 0.0
+            #Recombination and mutation
+            for group_size in recombination_split:
+                pool = populations[population_name][-group_size:]
+                random.shuffle(pool)
+                children = []
+                for i in range(0, group_size, 2):
+                    children.extend(reproduce(pool[i], pool[i+1]))
+                for child in children:
+                    new_population.append(mutate(child))
+            populations[population_name] = new_population
         print(f"Generation training time: {time.time() - generation_start_time}")
         generation += 1
     print(f"\nTotal time: {time.time() - start_time}")
